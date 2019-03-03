2a56275ecb38244ed1b360a016191a56aa2925ad
Shaun Lin <fashionshow0@gmail.com>
2019-03-03 16:15:32 +0800

    Create this_test.js

---
+++ this/this_test.js
@@ -0,0 +1,13 @@
+var foo = 'foo';
+var obj = {
+  foo: 'foo in Object'
+};
+
+var sayFoo = function() {
+  console.log( this.foo );
+};
+
+obj.sayFoo = sayFoo;
+
+obj.sayFoo();   // ?
+sayFoo();       // ?
