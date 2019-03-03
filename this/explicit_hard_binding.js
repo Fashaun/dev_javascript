2d70e93e6308cf6e2c23c8743e0df33535657419
Shaun Lin <fashionshow0@gmail.com>
2019-03-03 16:41:34 +0800

    Create explicit_hard_binding.js

---
+++ this/explicit_hard_binding.js
@@ -0,0 +1,26 @@
+function func() {
+  console.log( this.a );
+}
+
+var obj = {
+  a: 2
+};
+
+var obj2 = {
+  a: 100
+};
+
+// Hard binding function
+var hard_binding_func = function() {
+  func.call( obj );
+};
+
+func();                 // undefined
+func.call(obj);         // 2
+func.call(obj2);        // 100
+
+hard_binding_func();    // 2
+hard_binding_func.call(obj2);  // 2
+
+window.setTimeout( func, 10);  // undefined
+window.setTimeout( hard_binding_func, 10);  // 2
